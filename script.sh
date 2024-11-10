d8aed0ce7dd455c152d90ebc93c04decfd2bef11
zel-hach <zineb.ell.zee@gmail.com>
2024-11-10 19:18:28 +0100

    some modification at script

--- script.sh
+++ script.sh
@@ -9,8 +9,11 @@ echo "Clearing cache..."
 php bin/console cache:clear
 symfony server:start -d
 composer install -vvv
+npm install
+npm run dev
 echo "Running database migrations..."
 php bin/console cache:clear --verbose
+php bin/console cache:clear
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate
 echo "Starting Symfony server..."
